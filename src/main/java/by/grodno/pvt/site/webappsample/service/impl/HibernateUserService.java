97069098eb9657395910667ed4bdea9e57f07ebe
Maksim Naumovich <mnaumovich@inspirebrands.com>
2020-11-27 21:09:55 +0300

    Transactional annotation

--- src/main/java/by/grodno/pvt/site/webappsample/service/impl/HibernateUserService.java
+++ src/main/java/by/grodno/pvt/site/webappsample/service/impl/HibernateUserService.java
@@ -2,38 +2,45 @@ package by.grodno.pvt.site.webappsample.service.impl;
 
 import java.util.List;
 
+import javax.activity.InvalidActivityException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
-import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.pbkdf2.RuntimeCryptoException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import by.grodno.pvt.site.webappsample.domain.OldUser;
 import by.grodno.pvt.site.webappsample.service.UserRepository;
 
 @Service
+@Transactional(noRollbackFor = NullPointerException.class)
 public class HibernateUserService implements UserRepository {
 
 	@PersistenceContext
 	private EntityManager entityManager;
-	@Autowired
-	private TransactionTemplate txTemplate;
+
+	static Boolean thrown = false;
 
 	@Override
 	public void addUser(List<OldUser> users) {
+		int i = 0;
+		for (OldUser user : users) {
+			entityManager.persist(user);
+			i++;
 
-		txTemplate.execute(tx -> {
-			for (OldUser user : users) {
-				entityManager.persist(user);
+			if (i == 3 && thrown == false) {
+				thrown = true;
+				throw new IndexOutOfBoundsException();
+				
 			}
-			return null;
-		});
-
+			if (i == 3) {
+				thrown = true;
+				throw new NullPointerException();
+			}
+		}
 	}
 
 	@Override
@@ -49,10 +56,6 @@ public class HibernateUserService implements UserRepository {
 
 	@Override
 	public void deleteUser(Integer number) {
-
-		entityManager.getTransaction().begin();
 		entityManager.remove(new OldUser(number, null, null, null, false));
-		entityManager.getTransaction().commit();
-
 	}
 }
