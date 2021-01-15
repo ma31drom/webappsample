3aa21714be7a0fa7630c84b25f0a7283a9cd4663
Maksim Naumovich <mnaumovich@inspirebrands.com>
2021-01-15 20:30:04 +0300

    Dropdown sample

---
+++ src/main/java/by/grodno/pvt/site/webappsample/controller/DropDownSample.java
@@ -0,0 +1,27 @@
+package by.grodno.pvt.site.webappsample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import by.grodno.pvt.site.webappsample.domain.UserRole;
+import by.grodno.pvt.site.webappsample.repo.UserRepo;
+
+@Controller
+public class DropDownSample {
+
+	@GetMapping("/dropdown")
+	public String dropdown(Model model) {
+		model.addAttribute("currentRole", UserRole.TEACHER);
+		return "dropdownSample";
+	}
+
+	@PostMapping("/dropdown")
+	public String index(@RequestParam UserRole role) {
+		System.out.println();
+		return "redirect:/users";
+	}
+
+}
